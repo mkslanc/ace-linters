4375c2c6921078a400f05240afe1d3641bb0982a
anijanyan <amirjanyanani@gmail.com>
2022-12-06 17:03:20 +0400

    description tooltip fixed

--- src/language-provider.ts
+++ src/language-provider.ts
@@ -7,18 +7,21 @@ import {MessageType} from "./message-types";
 import * as oop from "ace-code/src/lib/oop";
 import {EventEmitter} from "ace-code/src/lib/event_emitter";
 import {ServiceOptions, Tooltip} from "./services/language-service";
+import {DescriptionTooltip} from "./components/description-tooltip";
 
 var showdown = require('showdown');
 
 export class LanguageProvider {
-    private $editor: Ace.Editor;
+    editor: Ace.Editor;
+    private $descriptionTooltip;
     markdownConverter: MarkDownConverter;
     message: MessageController;
     options: ServiceOptions;
     deltaQueue: Ace.Delta[] = [];
 
     constructor(editor: Ace.Editor, options: ServiceOptions, markdownConverter?: MarkDownConverter) {
-        this.$editor = editor;
+        this.editor = editor;
+        this.$descriptionTooltip = new DescriptionTooltip(this);
         this.options = options;
         this.$adaptOptions();
         if (markdownConverter) {
@@ -28,7 +31,7 @@ export class LanguageProvider {
         }
         this.$init();
         this.validate();
-        this.$editor.session.doc.on("change", this.$changeListener.bind(this), true);
+        this.editor.session.doc.on("change", this.$changeListener.bind(this), true);
     }
 
     private $changeListener(delta) {
@@ -47,11 +50,11 @@ export class LanguageProvider {
             this["off"]("changed", changed);
         };
         this["on"]("changed", changed);
-        this.message.change(this.$editor.session["id"], deltas, this.$editor.session.getValue(), this.$editor.session.doc.getLength());
+        this.message.change(this.editor.session["id"], deltas, this.editor.session.getValue(), this.editor.session.doc.getLength());
     };
 
     private $adaptOptions() {
-        let editorOptions = this.$editor.getOptions();
+        let editorOptions = this.editor.getOptions();
         this.options.mode = editorOptions.mode;
         if (!this.options.format) {
             this.options.format = {tabSize: null, insertSpaces: null};
@@ -62,7 +65,7 @@ export class LanguageProvider {
 
     private $init() {
         this.message = MessageController.instance;
-        this.message["on"]("message-" + this.$editor.session["id"], (e) => {
+        this.message["on"]("message-" + this.editor.session["id"], (e) => {
             let message = e.data;
             switch (message.type as MessageType) {
                 case MessageType.format:
@@ -87,29 +90,29 @@ export class LanguageProvider {
                     break;
             }
         });
-        this.message.init(this.$editor.session["id"], this.$editor.getValue(), this.options);
+        this.message.init(this.editor.session["id"], this.editor.getValue(), this.options);
         // @ts-ignore
-        this.$editor.on("changeMode", () => {
+        this.editor.on("changeMode", () => {
             this.$adaptOptions();
-            this.message.changeMode(this.$editor.session["id"], this.options);
+            this.message.changeMode(this.editor.session["id"], this.options);
             //TODO: validate after mode change?
         });
     }
 
     validate() {
-        this.message.doValidation(this.$editor.session["id"]);
+        this.message.doValidation(this.editor.session["id"]);
         var $setAnnotations = (annotations: Ace.Annotation[]) => {
             this["off"]("validate", $setAnnotations);
-            this.$editor.session.clearAnnotations();
+            this.editor.session.clearAnnotations();
             if (annotations && annotations.length > 0) {
-                this.$editor.session.setAnnotations(annotations);
+                this.editor.session.setAnnotations(annotations);
             }
         }
         this["on"]("validate", $setAnnotations);
     }
 
     doHover(position: Ace.Point) {
-        this.message.doHover(this.$editor.session["id"], position);
+        this.message.doHover(this.editor.session["id"], position);
     }
 
     getTooltipText(hover: Tooltip) {
@@ -120,31 +123,31 @@ export class LanguageProvider {
     }
 
     format() {
-        var row = this.$editor.session.getLength();
-        var column = this.$editor.session.getLine(row).length - 1;
-        var selectionRanges = this.$editor.getSelection().getAllRanges();
+        var row = this.editor.session.getLength();
+        var column = this.editor.session.getLine(row).length - 1;
+        var selectionRanges = this.editor.getSelection().getAllRanges();
         if (selectionRanges.length > 0 && !selectionRanges[0].isEmpty()) {
             for (var range of selectionRanges) {
-                this.message.format(this.$editor.session["id"], range);
+                this.message.format(this.editor.session["id"], range);
             }
         } else {
-            this.message.format(this.$editor.session["id"], new AceRange(0, 0, row, column));
+            this.message.format(this.editor.session["id"], new AceRange(0, 0, row, column));
         }
     }
 
     private $applyFormat(edits: TextEdit[]) {
         for (var edit of edits.reverse()) {
-            this.$editor.session.doc.replace(toRange(edit.range), edit.newText);
+            this.editor.session.doc.replace(toRange(edit.range), edit.newText);
         }
     }
 
     doComplete() {
-        let cursor = this.$editor.getCursorPosition();
-        this.message.doComplete(this.$editor.session["id"], cursor);
+        let cursor = this.editor.getCursorPosition();
+        this.message.doComplete(this.editor.session["id"], cursor);
     }
 
     registerCompleters() {
-        this.$editor.completers = [
+        this.editor.completers = [
             {
                 getCompletions: async (editor, session, pos, prefix, callback) => {
                     this.doComplete();
