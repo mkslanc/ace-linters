467985c49a60b2d78a1360ec8b5aa92778c6a3c7
anijanyan <amirjanyanani@gmail.com>
2022-12-09 13:35:38 +0400

    all received messages are posting answer messages

--- src/webworker.ts
+++ src/webworker.ts
@@ -9,30 +9,29 @@ ctx.onmessage = async (ev) => {
     let message = ev.data;
     let uri = message.sessionId;
     let manager = ServiceManager.instance;
+    let postMessage = {
+        "type": message.type,
+        "sessionId": uri,
+    };
     switch (message["type"] as MessageType) {
         case MessageType.format:
-            let edits = manager.getServiceInstance(uri).format(message.value);
-            ctx.postMessage({type: MessageType.format, sessionId: uri, edits: edits});
+            postMessage["edits"] = manager.getServiceInstance(uri).format(message.value, message.format);
             break;
         case MessageType.complete:
-            let completions = await manager.getServiceInstance(uri).doComplete(message.value);
-            ctx.postMessage({type: MessageType.complete, sessionId: uri, completions: completions});
+            postMessage["completions"] = await manager.getServiceInstance(uri).doComplete(message.value);
             break;
         case MessageType.change:
             manager.getServiceInstance(uri).setValue(message.value);
-            ctx.postMessage({type: MessageType.change, sessionId: uri});
             break;
         case MessageType.applyDelta:
             manager.getServiceInstance(uri).applyDeltas(message.value);
-            ctx.postMessage({type: MessageType.change, sessionId: uri});
+            postMessage["type"] = MessageType.change;
             break;
         case MessageType.hover:
-            let hover = await manager.getServiceInstance(uri).doHover(message.value);
-            ctx.postMessage({type: MessageType.hover, sessionId: uri, hover: hover});
+            postMessage["hover"] = await manager.getServiceInstance(uri).doHover(message.value);
             break;
         case MessageType.validate:
-            let annotations = await manager.getServiceInstance(uri).doValidation();
-            ctx.postMessage({type: MessageType.validate, sessionId: uri, annotations: annotations});
+            postMessage["annotations"] = await manager.getServiceInstance(uri).doValidation();
             break;
         case MessageType.init: //this should be first message
             let options: ServiceOptions = message.options;
@@ -47,4 +46,6 @@ ctx.onmessage = async (ev) => {
             await manager.addServiceInstance(uri, newDoc, newOptions);
             break;
     }
+
+    ctx.postMessage(postMessage);
 };
