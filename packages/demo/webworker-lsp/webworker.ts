b81f4c553528190e6255ab40c5ab922c8123eaf6
mkslanc <mkslanc@gmail.com>
2023-01-31 14:31:31 +0400

    return html for demo

--- packages/demo/webworker-lsp/webworker.ts
+++ packages/demo/webworker-lsp/webworker.ts
@@ -2,6 +2,12 @@ import {ServiceManager} from "ace-linters/services/service-manager";
 
 let manager = new ServiceManager(self);
 
+manager.registerService("html", {
+    module: () => import("ace-linters/services/html/html-service"),
+    className: "HtmlService",
+    modes: "html"
+});
+
 manager.registerService("css", {
     module: () => import("ace-linters/services/css/css-service"),
     className: "CssService",
