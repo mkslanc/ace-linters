f553aed11aee13d4645dd2c3c0e4ef9c1aceda63
mkslanc <mkslanc@gmail.com>
2023-05-23 20:39:05 +0400

    fix test

--- packages/ace-linters/tests/unit/common-converters.tests.ts
+++ packages/ace-linters/tests/unit/common-converters.tests.ts
@@ -7,20 +7,6 @@ import {CommonConverter} from "../../type-converters/common-converters";
 
 
 describe('General Converters', () => {
-    describe('normalizeRanges', () => {
-        it('should correctly normalize the ranges of an array of completions', () => {
-            const completions = [
-                {value: 'value1', range: {start: {row: 0, column: 0}, end: {row: 1, column: 0}}},
-                {value: 'value2', range: {start: {row: 2, column: 0}, end: {row: 3, column: 0}}}
-            ];
-            const expected = [
-                {value: 'value1', range: AceRange.fromPoints({row: 0, column: 0}, {row: 1, column: 0})},
-                {value: 'value2', range: AceRange.fromPoints({row: 2, column: 0}, {row: 3, column: 0})}
-            ];
-            expect(CommonConverter.normalizeRanges(completions)).to.deep.equal(expected);
-        });
-    });
-
     describe('cleanHtml', () => {
         it('should clean the HTML string by replacing <a> tags', () => {
             const html = "<a href='www.example.com'>link</a>";
@@ -33,12 +19,12 @@ describe('General Converters', () => {
         it('should correctly convert a range object to an Ace range', () => {
             const range = {start: {row: 0, column: 0}, end: {row: 1, column: 0}};
             const expected = AceRange.fromPoints({row: 0, column: 0}, {row: 1, column: 0});
-            expect(CommonConverter.toRange(range)).to.deep.equal(expected);
+            expect(CommonConverter.toRange(range, AceRange)).to.deep.equal(expected);
         });
 
         it('should return undefined if the range object is missing start or end properties', () => {
             const range = {start: {row: 0, column: 0}};
-            expect(CommonConverter.toRange(range as any)).to.undefined;
+            expect(CommonConverter.toRange(range as any, AceRange)).to.undefined;
         });
     });
 
