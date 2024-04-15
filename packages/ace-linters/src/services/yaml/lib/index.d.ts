696d2d94b208737d413d7ca89a48b9dbb2155108
mkslanc <mkslanc@gmail.com>
2024-04-15 13:16:12 +0400

    fix outdated `getLanguageService` yaml service param

---
+++ packages/ace-linters/src/services/yaml/lib/index.d.ts
@@ -0,0 +1,6 @@
+export {
+    getLanguageService
+} from "yaml-language-server/lib/esm/languageservice/yamlLanguageService";
+export { SettingsState } from "yaml-language-server/lib/esm/yamlSettings";
+export { YAMLSchemaService } from "yaml-language-server/lib/esm/languageservice/services/yamlSchemaService";
+export { YAMLValidation } from "yaml-language-server/lib/esm/languageservice/services/yamlValidation";
