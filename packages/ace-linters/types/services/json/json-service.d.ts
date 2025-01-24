4c76b38b33f80e8454072a7110bba5f10aca1ae2
mkslanc <mkslanc@gmail.com>
2025-01-24 15:57:33 +0400

    bump new version

--- packages/ace-linters/types/services/json/json-service.d.ts
+++ packages/ace-linters/types/services/json/json-service.d.ts
@@ -1,7 +1,6 @@
 import { LanguageService as JsonLanguageService, SchemaConfiguration } from "vscode-json-languageservice";
 import { BaseService } from "../base-service";
 import * as lsp from "vscode-languageserver-protocol";
-import * as jsonService from 'vscode-json-languageservice';
 import { TextDocumentIdentifier, TextDocumentItem } from "vscode-languageserver-protocol";
 import { JsonServiceOptions, LanguageService } from "../../types/language-service";
 export declare class JsonService extends BaseService<JsonServiceOptions> implements LanguageService {
@@ -34,7 +33,7 @@ export declare class JsonService extends BaseService<JsonServiceOptions> impleme
     removeDocument(document: TextDocumentIdentifier): void;
     setOptions(documentUri: string, options: JsonServiceOptions, merge?: boolean): void;
     setGlobalOptions(options: JsonServiceOptions): void;
-    format(document: lsp.TextDocumentIdentifier, range: lsp.Range, options: lsp.FormattingOptions): Promise<jsonService.TextEdit[]>;
+    format(document: lsp.TextDocumentIdentifier, range: lsp.Range, options: lsp.FormattingOptions): Promise<lsp.TextEdit[]>;
     doHover(document: lsp.TextDocumentIdentifier, position: lsp.Position): Promise<lsp.Hover | null>;
     doValidation(document: lsp.TextDocumentIdentifier): Promise<lsp.Diagnostic[]>;
     doComplete(document: lsp.TextDocumentIdentifier, position: lsp.Position): Promise<lsp.CompletionItem[] | lsp.CompletionList | null>;
