3d7bd6870c3ba8029450eb46eb766dfd975e0040
mkslanc <mkslanc@gmail.com>
2026-03-02 17:45:48 +0400

    feat: replace previous PHP parser integration with php-parser for diagnostics, implement `php-service` tests, and bump version to 2.1.1

--- packages/ace-linters/postbuild.js
+++ packages/ace-linters/postbuild.js
@@ -8,75 +8,91 @@ const commonOutputParams = {
 };
 
 const entries = [
-    {
-        filePath: './src/index.ts',
-        outFile: './build/ace-linters.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/service-manager.ts',
-        outFile: './build/service-manager.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/html/html-service.ts',
-        outFile: './build/html-service.d.ts',
-        output: commonOutputParams,
-        libraries: {
-            inlinedLibraries: ["vscode-html-languageservice"]
-        }
-    }, {
-        filePath: './src/services/css/css-service.ts',
-        outFile: './build/css-service.d.ts',
-        output: commonOutputParams,
-        libraries: {
-            inlinedLibraries: ["vscode-css-languageservice"]
-        }
-    }, {
-        filePath: './src/services/json/json-service.ts',
-        outFile: './build/json-service.d.ts',
-        output: commonOutputParams,
-        libraries: {
-            inlinedLibraries: ["vscode-json-languageservice"]
-        }
-    }, {
-        filePath: './src/services/lua/lua-service.ts',
-        outFile: './build/lua-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/typescript/typescript-service.ts',
-        outFile: './build/typescript-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/yaml/yaml-service.ts',
-        outFile: './build/yaml-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/xml/xml-service.ts',
-        outFile: './build/xml-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/php/php-service.ts',
-        outFile: './build/php-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/ace-language-client.ts',
-        outFile: './build/ace-language-client.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/javascript/javascript-service.ts',
-        outFile: './build/javascript-service.d.ts',
-        output: commonOutputParams,
-        libraries: {
-            inlinedLibraries: ["eslint"]
-        }
-    }, {
-        filePath: './src/services/base-service.ts',
-        outFile: './build/base-service.d.ts',
-        output: commonOutputParams
-    }, {
-        filePath: './src/services/language-client.ts',
-        outFile: './build/language-client.d.ts',
-        output: commonOutputParams
-    }
+  {
+    filePath: "./src/index.ts",
+    outFile: "./build/ace-linters.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/service-manager.ts",
+    outFile: "./build/service-manager.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/html/html-service.ts",
+    outFile: "./build/html-service.d.ts",
+    output: commonOutputParams,
+    libraries: {
+      inlinedLibraries: ["vscode-html-languageservice"],
+    },
+  },
+  {
+    filePath: "./src/services/css/css-service.ts",
+    outFile: "./build/css-service.d.ts",
+    output: commonOutputParams,
+    libraries: {
+      inlinedLibraries: ["vscode-css-languageservice"],
+    },
+  },
+  {
+    filePath: "./src/services/json/json-service.ts",
+    outFile: "./build/json-service.d.ts",
+    output: commonOutputParams,
+    libraries: {
+      inlinedLibraries: ["vscode-json-languageservice"],
+    },
+  },
+  {
+    filePath: "./src/services/lua/lua-service.ts",
+    outFile: "./build/lua-service.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/typescript/typescript-service.ts",
+    outFile: "./build/typescript-service.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/yaml/yaml-service.ts",
+    outFile: "./build/yaml-service.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/xml/xml-service.ts",
+    outFile: "./build/xml-service.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/php/php-service.ts",
+    outFile: "./build/php-service.d.ts",
+    output: commonOutputParams,
+    libraries: {
+      inlinedLibraries: ["php-parser"],
+    },
+  },
+  {
+    filePath: "./src/ace-language-client.ts",
+    outFile: "./build/ace-language-client.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/javascript/javascript-service.ts",
+    outFile: "./build/javascript-service.d.ts",
+    output: commonOutputParams,
+    libraries: {
+      inlinedLibraries: ["eslint"],
+    },
+  },
+  {
+    filePath: "./src/services/base-service.ts",
+    outFile: "./build/base-service.d.ts",
+    output: commonOutputParams,
+  },
+  {
+    filePath: "./src/services/language-client.ts",
+    outFile: "./build/language-client.d.ts",
+    output: commonOutputParams,
+  },
 ];
 
 const bundles = generateDtsBundle(entries, {preferredConfigPath: './tsconfig.json'});
